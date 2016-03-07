d17bb83b8a37f6d8852eb19f0701a848a9519777
Aaron Tsai <aattsai@gmail.com>
2016-03-07 12:20:15 -0500

    Add ratings migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160307171311) do
+ActiveRecord::Schema.define(version: 20160307171555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,16 @@ ActiveRecord::Schema.define(version: 20160307171311) do
   end
 
   add_index "performers", ["username"], name: "index_performers_on_username", using: :btree
+
+  create_table "ratings", force: :cascade do |t|
+    t.float    "value",        null: false
+    t.string   "comment"
+    t.integer  "user_id",      null: false
+    t.integer  "performer_id"
+    t.integer  "band_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username",        null: false
