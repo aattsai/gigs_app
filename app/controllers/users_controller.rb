10216a1d87b589381b15f1cbafe8fd46545361cf
Aaron Tsai <aattsai@gmail.com>
2016-03-09 12:19:48 -0500

    Remove debugger

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,6 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
-      debugger
       redirect_to root_path
     else
       flash[:warning] = user.errors.full_messages.join(". ")
