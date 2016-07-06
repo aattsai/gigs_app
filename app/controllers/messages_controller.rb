cd0817a82f222724e9f018b9d30c82c31a91d78a
Jack Huang <jack7100@gmail.com>
2016-07-06 09:27:55 -0400

    Fix message not being saved issue

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -13,22 +13,26 @@ class MessagesController < ApplicationController
     @messages = @conversation.messages
     if current_user.class == User 
       @message = Message.new(conversation: @conversation, user: current_user, content: params[:message][:content])
-      PrivatePub.publish_to(conversation_messages_path(@conversation), "#{@message}")
-      respond_to do |format|
-        format.js 
+      if @message.save
+        PrivatePub.publish_to(conversation_messages_path(@conversation), "#{@message}")
+        respond_to do |format|
+          format.js 
+        end
+      else
+        render :show
       end
     elsif current_user.class == Performer
       @message = Message.new(conversation: @conversation, performer: current_user, content: params[:message][:content])
-      PrivatePub.publish_to(conversation_messages_path(@conversation), "#{@message}")
-      respond_to do |format|
-        format.js 
+      if @message.save
+        PrivatePub.publish_to(conversation_messages_path(@conversation), "#{@message}")
+        respond_to do |format|
+          format.js 
+        end
+      else
+        render :show
       end
+
     end
-    # if @message.save
-    #   render partial: 'message', locals: {messages: @messages}
-    # else
-    #   render :show
-    # end
   end
   
 
